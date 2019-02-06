5d9c838df009ff968f2951e194d67d07e72c74f8
Rhicheek Patra <rhicheek.patra@oracle.com>
2019-02-06 16:20:20 +0100

    Few optimizations

--- graphlet-representation/graphlet-classifier/classifier.py
+++ graphlet-representation/graphlet-classifier/classifier.py
@@ -14,12 +14,12 @@ from os.path import dirname
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 tf.logging.set_verbosity(tf.logging.ERROR)
 
-def convertOneHot(data,n_classes):
-    y=np.array([int(i[0]) for i in data])
-    y_onehot=[0]*len(y)
+def oneHotEncoder(data, n_classes):
+    y = np.array([int(i[0]) for i in data])
+    y_onehot = [0]*len(y)
     for i,j in enumerate(y):
-        y_onehot[i]=[0]*n_classes
-        y_onehot[i][j]=1
+        y_onehot[i] = [0]*n_classes
+        y_onehot[i][j]= 1
     return (y,y_onehot)
 
 # model parameters
@@ -35,8 +35,7 @@ batch_size = 256
 dropout = 0.5 # probability to keep units (for dropout)
 seed = 0
 
-# display parameters
-display_step = 10
+# display test metrics
 test_step = 10
 
 # set random seed for reproducability
@@ -50,10 +49,10 @@ test_data = genfromtxt(data_dir+"NCI09_test.csv", delimiter=',')  # Training dat
 
 
 x_train = np.array([ i[1::] for i in train_data])
-y_train, y_train_onehot = convertOneHot(train_data, n_classes)
+y_train, y_train_onehot = oneHotEncoder(train_data, n_classes)
 
 x_test = np.array([ i[1::] for i in test_data])
-y_test, y_test_onehot = convertOneHot(test_data, n_classes)
+y_test, y_test_onehot = oneHotEncoder(test_data, n_classes)
 
 # tf Graph input
 x = tf.placeholder(tf.float32, [None, n_input])
@@ -76,40 +75,40 @@ def pool2d(x, k=2):
 def conv_net(x, keep_prob):
     # Store layers weight & bias
     weights = {
-        'wc1': tf.Variable(tf.random_normal([5, 5, 1, 32], seed=seed)),
-        'wc2': tf.Variable(tf.random_normal([5, 5, 32, 64], seed=seed)),
-        'wd1': tf.Variable(tf.random_normal([shape1*shape2*64, 1024], seed=seed)),
-        'out': tf.Variable(tf.random_normal([1024, n_classes], seed=seed))
+        'conv_layer1': tf.Variable(tf.random_normal([5, 5, 1, 32], seed=seed)),
+        'conv_layer2': tf.Variable(tf.random_normal([5, 5, 32, 64], seed=seed)),
+        'dense_layer': tf.Variable(tf.random_normal([shape1*shape2*64, 1024], seed=seed)),
+        'output_layer': tf.Variable(tf.random_normal([1024, n_classes], seed=seed))
     }
 
     biases = {
-        'bc1': tf.Variable(tf.random_normal([32], seed=seed)),
-        'bc2': tf.Variable(tf.random_normal([64], seed=seed)),
-        'bd1': tf.Variable(tf.random_normal([1024], seed=seed)),
-        'out': tf.Variable(tf.random_normal([n_classes], seed=seed))
+        'conv_layer1': tf.Variable(tf.random_normal([32], seed=seed)),
+        'conv_layer2': tf.Variable(tf.random_normal([64], seed=seed)),
+        'dense_layer': tf.Variable(tf.random_normal([1024], seed=seed)),
+        'output_layer': tf.Variable(tf.random_normal([n_classes], seed=seed))
     }
 
     # Reshape input picture
     x = tf.reshape(x, shape=[-1, shape1, shape2, 1])
 
     # Convolution Layer
-    conv1 = conv2d(x, weights['wc1'], biases['bc1'])
+    conv1 = conv2d(x, weights['conv_layer1'], biases['conv_layer1'])
     # Max Pooling (down-sampling)
     conv1 = pool2d(conv1, k=1)
 
     # Convolution Layer
-    conv2 = conv2d(conv1, weights['wc2'], biases['bc2'])
+    conv2 = conv2d(conv1, weights['conv_layer2'], biases['conv_layer2'])
     # Max Pooling (down-sampling)
     conv2 = pool2d(conv2, k=1)
 
-    fc1 = tf.reshape(conv2, [-1, weights['wd1'].get_shape().as_list()[0]])
-    fc1 = tf.add(tf.matmul(fc1, weights['wd1']), biases['bd1'])
+    fc1 = tf.reshape(conv2, [-1, weights['dense_layer'].get_shape().as_list()[0]])
+    fc1 = tf.add(tf.matmul(fc1, weights['dense_layer']), biases['dense_layer'])
     fc1 = tf.nn.relu(fc1)
     # Apply Dropout
     fc1 = tf.nn.dropout(fc1, keep_prob, seed=seed)
 
     # Output, class prediction
-    out = tf.add(tf.matmul(fc1, weights['out']), biases['out'])
+    out = tf.add(tf.matmul(fc1, weights['output_layer']), biases['output_layer'])
     return out
 
 
@@ -142,13 +141,10 @@ with tf.Session() as sess:
         batch_y = np.asarray(y_train_onehot)[idx,:]
         # run optimization op (backprop)
         sess.run(optimizer, feed_dict={x: batch_x, y: batch_y, keep_prob: dropout})
-        if step % display_step == 0:
-            # calculate batch loss and accuracy
-            loss, acc = sess.run([cost, accuracy], feed_dict={x: batch_x, y: batch_y, keep_prob: 1.})
-            if step % test_step ==0:
-                # calculate accuracy for test data
-                loss, acc = sess.run([cost, accuracy], feed_dict={x: x_test, y: np.asarray(y_test_onehot), keep_prob: 1.})
-                print("Iterations: %s, Test Accuracy: %f" % (str(step*batch_size),acc))
+        if step % test_step == 0:
+            # calculate accuracy for test data
+            loss, acc = sess.run([cost, accuracy], feed_dict={x: x_test, y: np.asarray(y_test_onehot), keep_prob: 1.})
+            print("Iterations: %s, Test Accuracy: %f" % (str(step*batch_size),acc))
         step += 1
 
 print("Complete!")
